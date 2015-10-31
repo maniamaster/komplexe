75b3f50af66ecb137307f6b4192b3acf81ca56df
maniamaster <eric.bertok@gmail.com>
2015-10-31 14:47:50 +0100

    finalized volterra

--- zettel 1/lotka volterra.py
+++ zettel 1/lotka volterra.py
@@ -13,12 +13,12 @@ import matplotlib.pyplot as plt
 # define system in terms of a Numpy array
 def Sys(X, t=0):
     # here X[0] = x and x[1] = y    
-    return np.array([ 2*(1-X[0])*X[0] - X[0]*X[1] , -0.5*X[1] + X[0]*X[1] ])
+    return np.array([ (1-X[0])*X[0] - 10*X[0]*X[1] , -1.5*X[1] + 10*X[0]*X[1] ])
 
 # generate 1000 linearly spaced numbers for x-axes
-t = np.linspace(0, 20,  1000)
-# initial values: x0 = 10, y0 = 2
-Sys0 = np.array([1, 3])
+t = np.linspace(0, 50,  2500)
+# initial values: x0 = 2, y0 = 6
+Sys0 = np.array([0.9, 0.1])
 
 # type "help(integrate.odeint)" if you want more information about integrate.odeint inputs and outputs.
 X, infodict = integrate.odeint(Sys, Sys0, t, full_output=True)
@@ -32,8 +32,8 @@ fig.subplots_adjust(wspace = 0.5, hspace = 0.3)
 ax1 = fig.add_subplot(1,2,1)
 ax2 = fig.add_subplot(1,2,2)
 
-ax1.plot(x, 'r-', label='predator')
-ax1.plot(y, 'b-', label='prey')
+ax1.plot(x, 'r-', label='prey')
+ax1.plot(y, 'b-', label='predator')
 ax1.set_title("Dynamics in time")
 ax1.set_xlabel("time")
 ax1.grid()
@@ -45,12 +45,12 @@ ax2.set_ylabel("y")
 ax2.set_title("Phase space")
 ax2.grid()
 
-# quiverplot
+# streamplot
 # define a grid and compute direction at each point
-x = np.linspace(0, 2, 20)
-y = np.linspace(0, 2, 20)
+x_p = np.linspace(0, 2, 20)
+y_p = np.linspace(0, 2, 20)
 
-X1 , Y1  = np.meshgrid(x, y)                    # create a grid
+X1 , Y1  = np.meshgrid(x_p, y_p)                    # create a grid
 DX1, DY1 = Sys([X1, Y1])                        # compute growth rate on the grid
 
 #plot
