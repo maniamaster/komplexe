15befd52bfcce716aad0e2099199ced8e5aa9a37
Eric Bertok <eric.bertok@c097.cip.physik.lokal>
2016-01-14 17:56:31 +0100

    Zettel10 aufg1 fertig

--- zettel 10/kuramoto.py
+++ zettel 10/kuramoto.py
@@ -14,9 +14,10 @@ import time
 #Parameters:
 N=10
 stdev=0.1
-K=1
-timee=1000
+K=0.2
+timee=2000
 dt=0.1
+anzK=1000
 #K=np.linspace(0,10,100)
 
 array=np.empty(N)
@@ -24,6 +25,7 @@ omega=np.empty(N)
 X=np.empty(N)
 x_0=2*np.pi*np.random.random(size=N)
 result=np.array([x_0,x_0])
+Rresult=np.array([])
 
 print result
 
@@ -46,28 +48,125 @@ def integrieren(x_0,result,K,omega,timee,dt=0.1):
         theta=np.angle(1/float(N)*np.sum(np.exp(1.j*X)))  
         R=np.abs(1/float(N)*np.sum(np.exp(1.j*X)))
         X=r.integrate(r.t+dt)
-        result=np.append(result,np.array([X]),axis=0)   
+        result=np.append(result,np.array([X]),axis=0)  
+        #Rresult=np.append(Rresult,R)
     return result
+
+def Rintegrieren(x_0,Rresult,K,omega,timee,dt=0.1):
+    global theta,R
+    r=sp.ode(Sys) 
+    r.set_integrator('dopri5')
+    r.set_f_params(K,omega)
+    r.set_initial_value(x_0)
+    X=x_0
+    while r.t<timee:
+        theta=np.angle(1/float(N)*np.sum(np.exp(1.j*X)))  
+        R=np.abs(1/float(N)*np.sum(np.exp(1.j*X)))
+        X=r.integrate(r.t+dt)
+        Rresult=np.append(Rresult,R)
+    Rresult=np.sum(Rresult)/(timee/dt)
+    return Rresult
+    
 
 start = time.time()
 ########################################################
 
-result=integrieren(x_0,result,K,omega,timee,dt)
 
-plt.ioff()
-for j in range(int(timee/dt)):
-    plt.scatter(sin(result[j,:]),cos(result[j,:]),c=np.array(range(N)))
-    plt.xlim(-1.5,1.5)
-    plt.ylim(-1.5,1.5)
-    plt.title('t= '+str(j)+' , K= '+str(K)+' , N= '+str(N)+' , stdev= '+str(stdev))
-    plt.savefig("./bilder/t{}.png".format(j),dpi=200)
-    plt.close()
+
+plt.ion()
+Rresult=np.array([])
+
+N=10
+fig=plt.figure()
+for K in range(anzK):
+    Rresult=Rintegrieren(x_0,Rresult,K/float(anzK),omega,timee,dt)  
+    K=K/float(anzK)
+    plt.plot(K,Rresult,'b.')
+    Rresult=np.array([])
+plt.title('N= '+str(N)+' , stdev= '+str(stdev))
+plt.xlabel('K')
+plt.ylabel('<R>')
+plt.savefig("stddev=0.1, N={}.png".format(N),dpi=200)
+
+N=100
+fig=plt.figure()
+for K in range(anzK):
+    Rresult=Rintegrieren(x_0,Rresult,K/float(anzK),omega,timee,dt)  
+    K=K/float(anzK)
+    plt.plot(K,Rresult,'b.')
+    Rresult=np.array([])
+plt.title('N= '+str(N)+' , stdev= '+str(stdev))
+plt.xlabel('K')
+plt.ylabel('<R>')
+plt.savefig("stddev=0.1, N={}.png".format(N),dpi=200)
+
+N=1000
+fig=plt.figure()
+for K in range(anzK):
+    Rresult=Rintegrieren(x_0,Rresult,K/float(anzK),omega,timee,dt)  
+    K=K/float(anzK)
+    plt.plot(K,Rresult,'b.')
+    Rresult=np.array([])
+plt.title('N= '+str(N)+' , stdev= '+str(stdev))
+plt.xlabel('K')
+plt.ylabel('<R>')
+plt.savefig("stddev=0.1, N={}.png".format(N),dpi=200)
+
+stdev=0.2
+
+N=10
+fig=plt.figure()
+for K in range(anzK):
+    Rresult=Rintegrieren(x_0,Rresult,K/float(anzK),omega,timee,dt)  
+    K=K/float(anzK)
+    plt.plot(K,Rresult,'b.')
+    Rresult=np.array([])
+plt.title('N= '+str(N)+' , stdev= '+str(stdev))
+plt.xlabel('K')
+plt.ylabel('<R>')
+plt.savefig("stddev=0.2, N={}.png".format(N),dpi=200)
+
+N=100
+fig=plt.figure()
+for K in range(anzK):
+    Rresult=Rintegrieren(x_0,Rresult,K/float(anzK),omega,timee,dt)  
+    K=K/float(anzK)
+    plt.plot(K,Rresult,'b.')
+    Rresult=np.array([])
+plt.title('N= '+str(N)+' , stdev= '+str(stdev))
+plt.xlabel('K')
+plt.ylabel('<R>')
+plt.savefig("stddev=0.2, N={}.png".format(N),dpi=200)
+
+N=1000
+fig=plt.figure()
+for K in range(anzK):
+    Rresult=Rintegrieren(x_0,Rresult,K/float(anzK),omega,timee,dt)  
+    K=K/float(anzK)
+    plt.plot(K,Rresult,'b.')
+    Rresult=np.array([])
+plt.title('N= '+str(N)+' , stdev= '+str(stdev))
+plt.xlabel('K')
+plt.ylabel('<R>')
+plt.savefig("stddev=0.2, N={}.png".format(N),dpi=200)
+
+
+
+
+
+#########Animation:
+#result=integrieren(x_0,result,K,omega,timee,dt)
+#plt.ioff()
+#for j in range(int(timee/dt)):
+  #  plt.scatter(sin(result[j,:]),cos(result[j,:]),c=np.array(range(N)))
+  #  plt.xlim(-1.5,1.5)
+  #  plt.ylim(-1.5,1.5)
+  #  plt.title('t= '+str(j)+' , K= '+str(K)+' , N= '+str(N)+' , stdev= '+str(stdev))
+  #  plt.savefig("./bilder/t{}.png".format(j),dpi=200)
+  #  plt.close()
 ###animation:
 
 ########################################################
-x.fill(1)
-print x
-print result[:,5]
 stop = time.time()
 print stop-start,'\t'
 
