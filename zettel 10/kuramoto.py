4209f7f8d46ed4bb11b952da88a9903c3950cf55
Eric Bertok <eric.bertok@c097.cip.physik.lokal>
2016-01-14 14:17:47 +0100

    zettel 10 weiter

--- zettel 10/kuramoto.py
+++ zettel 10/kuramoto.py
@@ -14,7 +14,7 @@ import time
 #Parameters:
 N=10
 stdev=0.1
-K=10
+K=1
 timee=1000
 dt=0.1
 #K=np.linspace(0,10,100)
@@ -24,42 +24,50 @@ omega=np.empty(N)
 X=np.empty(N)
 x_0=2*np.pi*np.random.random(size=N)
 result=np.array([x_0,x_0])
-R=np.abs(1/float(N)*np.sum(np.exp(1.j*X)))
-theta=np.angle(1/float(N)*np.sum(np.exp(1.j*X)))
 
-for i in range(N):
-    omega[i]=np.random.normal(loc=1,scale=stdev)
-    array[i]=(omega[i]+K*np.sin(theta-X[i]))%(2*np.pi)
+print result
+
+omega=np.sort(np.random.normal(loc=1,scale=stdev,size=N))
 
 #Set Diff. Equation:
-def Sys(t,X):    
-    return np.array(np.fromiter(array,float))
+def Sys(t,X,K,omega):  
+    global theta,R
+    return np.array(omega+K*np.sin(theta-X))
 
 #integrate and check function:
-def integrieren(x_0,result,timee,dt=0.1):
+def integrieren(x_0,result,K,omega,timee,dt=0.1):
+    global theta,R
     r=sp.ode(Sys) 
     r.set_integrator('dopri5')
+    r.set_f_params(K,omega)
     r.set_initial_value(x_0)
+    X=x_0
     while r.t<timee:
-        print 'test'
+        theta=np.angle(1/float(N)*np.sum(np.exp(1.j*X)))  
+        R=np.abs(1/float(N)*np.sum(np.exp(1.j*X)))
         X=r.integrate(r.t+dt)
-        np.append(result,X)
+        result=np.append(result,np.array([X]),axis=0)   
     return result
 
 start = time.time()
 ########################################################
 
-result2=integrieren(x_0,result,timee,dt)
+result=integrieren(x_0,result,K,omega,timee,dt)
 
-fig=plt.figure()
-x=np.empty(N)
-#for j in range(int(timee/dt)):
- #   x.fill(j)
- #   plt.plot(result[j],x)
+plt.ioff()
+for j in range(int(timee/dt)):
+    plt.scatter(sin(result[j,:]),cos(result[j,:]),c=np.array(range(N)))
+    plt.xlim(-1.5,1.5)
+    plt.ylim(-1.5,1.5)
+    plt.title('t= '+str(j)+' , K= '+str(K)+' , N= '+str(N)+' , stdev= '+str(stdev))
+    plt.savefig("./bilder/t{}.png".format(j),dpi=200)
+    plt.close()
+###animation:
+
 ########################################################
 x.fill(1)
 print x
-print result2[199]
+print result[:,5]
 stop = time.time()
 print stop-start,'\t'
 
