56b93961566bf91c489ff7ac72d9e60d5c9793d0
miallo <mial.lohmann@gmail.com>
2016-01-22 10:51:09 +0100

    Bilder funktionieren jetzt

--- zettel 11/z11a1.py
+++ zettel 11/z11a1.py
@@ -2,35 +2,41 @@ import numpy as np
 import scipy as sp
 import matplotlib as mt
 import scipy.integrate as int
-#mt.use('Agg')
+# mt.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from pylab import *
 import sys
 import time
 
-#params:
-N=10000
-x0=0.3
+# params:
+N = 100000
+x0 = 0.3
 
-start=time.time()
+start = time.time()
+
 
 def tent(x):
-    return 2* np.min([x,1-x])*np.random.normal(loc=1, scale=0.01)
+    if x < 0.5:
+        return (2*x*np.random.normal(loc=1, scale=1e-4)) % 1
+    else:
+        return (2*(1-x)*np.random.normal(loc=1, scale=1e-4)) % 1
 
-x=x0
-arr=np.array([x0])
+x = x0
+arr = np.array([x0])
 for i in range(N):
-    x=tent(x)
-    arr=np.append(arr,x)
+    x = tent(x)
+    arr = np.append(arr, x)
 
-fourier=np.fft.fft(arr)
+fourier = np.fft.fft(arr)
 
-fig=plt.figure()
-axx=np.linspace(0,1,1000)
-plt.plot(fourier,'b.')
+fig = plt.figure()
+axx = np.linspace(0, 1, 1000)
+freqn   = np.fft.fftfreq(np.int(N+1))
+plt.plot(freqn[np.nonzero(freqn)],fourier[np.nonzero(freqn)], 'b,')
 plt.title('N='+str(N))
-plt.savefig('N='+str(N)+'.png',dpi=500)
+plt.show()
+#plt.savefig('N='+str(N)+'.png',dpi=500)
 
 stop = time.time()
-#print stop-start'\t'
+# print stop-start'\t'
